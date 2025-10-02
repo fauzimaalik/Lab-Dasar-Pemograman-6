f23478ca6df0815f5e0adcd76b55cd60d153f098
fauzimaalik <fauzimaalik3@gmail.com>
2025-10-02 15:38:26 +0700

    Add files via upload

---
+++ pertemuan 3/operasi_kondisi.pas
@@ -0,0 +1,73 @@
+uses crt;
+
+var 
+    nilai : integer;
+
+
+begin
+    clrscr;
+
+    write('masukkan nilai : '); readln(nilai);
+
+    // statement if 
+    // if(nilai <= 65) then
+    //     writeln('anda tidak lulus');
+
+    //statement if else
+    // if(nilai <= 65) then
+    //     begin
+    //         writeln('anda tidak lulus')
+    //     end
+    // else
+    //     begin
+    //         writeln('anda lulus!');
+    //     end;
+
+    // statement tiga kondisi
+    // if(nilai = 100)then
+    //     begin
+    //         writeln('perfect')
+    //     end
+    // else if(nilai <=65 ) then 
+    //     begin
+    //          writeln('anda tidak lulus')
+    //     end
+    // else
+    //     begin
+    //         writeln('anda lulus')
+    //     end;
+
+    // nested if
+    // if (nilai <=65) then
+    //     begin
+    //         writeln('anda tidak lulus')
+    //     end
+
+    // else
+    //     begin
+    //         if (nilai =100)then
+    //         begin
+    //              writeln('anda lulus dan hebat!')
+    //         end
+    //     else
+    //         begin
+    //             writeln('anda lulus')
+    //         end
+
+    //     end;
+
+    //statement case of
+    // case nilai of
+    //     1: writeln('senin');
+    //     2: writeln('selasa');
+    //     3: writeln('rabu');
+    //     4: writeln('kamis');
+    //     5: writeln('jumat');
+    //     6: writeln('sabtu');
+    //     7: writeln('ahad');
+    // else
+    //     writeln('inputan tidak VALID!');
+    // end;
+
+
+    end.

f61acd5eec8e5335c28e74ecd7097d3875b0e0c2
fauzimaalik <fauzimaalik3@gmail.com>
2025-10-30 08:27:39 +0700

    Add files via upload

---
+++ pertemuan 4/perloncatan.pas
@@ -0,0 +1,16 @@
+uses crt;
+label a, b, c, d, e;
+
+begin 
+clrscr;
+    a:writeln('hello world');
+    goto b;
+    d:writeln('Fasilkom-TI');
+    goto e;
+    c:writeln('Ilmu Komputer');
+    goto d;
+    b:writeln('IKLC');
+    goto c;
+    e:
+
+end.
